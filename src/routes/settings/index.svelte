6d519cd5b8dcbd02c25bdf35d956afc87c8c2104
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2022-09-13 14:56:27 -0400

    Setting: Postpone notification settings.

--- src/routes/settings/index.svelte
+++ src/routes/settings/index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import GeneralSettings from './_cpnt/_general/_general.svelte';
   import SecuritySettings from './_cpnt/_security/_security.svelte';
-  import NotificationSetttings from './_cpnt/_notification/_notification.svelte';
   import { getContext, onDestroy, setContext } from 'svelte';
   import { writable } from 'svelte/store';
   import MainViewNav from './_cpnt/_main-view-nav.svelte';
@@ -14,11 +13,11 @@
   const mainViewOptions: { id: string; view: ViewType; text: string }[] = [
     { id: 'general', view: { component: GeneralSettings, props: {} }, text: 'General' },
     { id: 'security', view: { component: SecuritySettings, props: {} }, text: 'Security' },
-    {
-      id: 'notification',
-      view: { component: NotificationSetttings, props: {} },
-      text: 'Notification',
-    },
+    // {
+    //   id: 'notification',
+    //   view: { component: NotificationSetttings, props: {} },
+    //   text: 'Notification',
+    // },
   ];
 
   setContext('mainView', writable<ViewType>(mainViewOptions[0].view));
