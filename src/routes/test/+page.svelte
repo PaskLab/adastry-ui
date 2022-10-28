eac5e47bdb17f1823422f5dddc0ff928e9b66df8
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2022-10-27 21:44:52 -0400

    Added comments.

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	let result;
+	import { browser } from '$app/environment';
+	let result = '';
+
+	/**
+	 * Because of server rendering, you have to make sure that the call is made
+	 * only in browser. You have 2 options, using the helper "browser",
+	 * or running the fetch call in the onMount method.
+	 *
+	 * Also, be mindful that you need to "guard/test" against undefined value in the
+	 * view as it will be pre-rendered on the server.
+	 */
+
+	// Using the "browser" helper.
+	if (browser) {
+		const test = fetch('/api/ada-seal?apiKey=adalovelace', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				accept: 'application/json'
+			}
+		});
+	}
+
+	// Using the "onMount". "onMount" is only called in the browser once the DOM is completely generated.
 	onMount(async () => {
 		const response = await fetch('/api/ada-seal?apiKey=adalovelace', {
 			method: 'GET',
