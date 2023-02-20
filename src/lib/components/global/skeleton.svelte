3ed6cfcfb74fa7ef7bd91861bf84615c358cfdcc
pascallapointe <3847945+pascallapointe@users.noreply.github.com>
2023-02-20 14:36:10 -0500

    Skeleton: Fixed wrong store usage.

--- src/lib/components/global/skeleton.svelte
+++ src/lib/components/global/skeleton.svelte
@@ -4,8 +4,8 @@
   export let width = '100%';
   export let height = '25px';
   export let borderRadius = '4px';
-  export let baseColor = darkMode ? 'rgb(38, 38, 52)' : 'rgb(238, 238, 238)';
-  export let highlightColor = darkMode ? 'rgb(72, 72, 80)' : 'rgb(245, 245, 245)';
+  export let baseColor = $darkMode ? 'rgb(38, 38, 52)' : 'rgb(238, 238, 238)';
+  export let highlightColor = $darkMode ? 'rgb(72, 72, 80)' : 'rgb(245, 245, 245)';
   export let animationLength = '1.2s';
 </script>
 
